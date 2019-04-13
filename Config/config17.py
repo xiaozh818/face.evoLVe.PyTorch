678809a34e56da9f976445af13b0a267a767626a
ydc123 <892431401@qq.com>
2019-04-13 12:33:47 -0400

    add config 16~18

--- Config/config17.py
+++ Config/config17.py
@@ -6,12 +6,12 @@ configurations = {
         SEED = 1337, # random seed for reproduce results
 
         DATA_ROOT = '../casia-maxpy-clean', # the parent root where your train/val/test data are stored
-        MODEL_ROOT = './model/model16', # the root to buffer your checkpoints
-        LOG_ROOT = 'log/log16', # the root to log your train/val status
+        MODEL_ROOT = './model/model17', # the root to buffer your checkpoints
+        LOG_ROOT = 'log/log17', # the root to log your train/val status
         BACKBONE_RESUME_ROOT = './', # the root to resume training from a saved checkpoint
         HEAD_RESUME_ROOT = './', # the root to resume training from a saved checkpoint
 
-        BACKBONE_NAME = 'ResNet_50', # support: ['mobileface', 'ShuffleNet', 'ShuffleNetV2', 'ResNet_50', 'ResNet_101', 'ResNet_152', 'IR_50', 'IR_101', 'IR_152', 'IR_SE_50', 'IR_SE_101', 'IR_SE_152']
+        BACKBONE_NAME = 'ShuffleNet', # support: ['mobileface', 'ShuffleNet', 'ShuffleNetV2', 'ResNet_50', 'ResNet_101', 'ResNet_152', 'IR_50', 'IR_101', 'IR_152', 'IR_SE_50', 'IR_SE_101', 'IR_SE_152']
         HEAD_NAME = 'CosFace', # support:  ['Softmax', 'ArcFace', 'CosFace', 'SphereFace', 'Am_softmax']
         LOSS_NAME = 'Softmax', # support: ['Focal', 'Softmax']
 
@@ -27,9 +27,9 @@ configurations = {
         MOMENTUM = 0.9,
         STAGES = [35, 65, 95], # epoch stages to decay learning rate
 
-        DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
+        DEVICE = torch.device("cuda:2" if torch.cuda.is_available() else "cpu"),
         MULTI_GPU = True, # flag to use multiple GPUs; if you choose to train with single GPU, you should first run "export CUDA_VISILE_DEVICES=device_id" to specify the GPU card you want to use
-        GPU_ID = [0, 1], # specify your GPU ids
+        GPU_ID = [2, 3], # specify your GPU ids
         PIN_MEMORY = True,
         NUM_WORKERS = 8,
 ),
