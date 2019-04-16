d086d00dbb7c3388c9759640bd3c0cb0122fcb8a
ShawnXYang <45842793+ShawnXYang@users.noreply.github.com>
2019-04-16 16:12:17 +0800

    Add files via upload

---
+++ loss/center_loss.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import
+import torch
+from torch import nn
+
+class CenterLoss(nn.Module):
+    def __init__(self, num_classes=10, feat_dim=1024, use_gpu=True):
+        super(CenterLoss, self).__init__()
+        self.num_classes = num_classes
+        self.feat_dim = feat_dim
+        self.use_gpu = use_gpu
+
+        if self.use_gpu:
+            self.centers = nn.Parameter(torch.randn(self.num_classes, self.feat_dim).cuda())
+        else:
+            self.centers = nn.Parameter(torch.randn(self.num_classes, self.feat_dim))
+
+    def forward(self, x, labels):
+        
+        batch_size = x.size(0)
+        distmat = torch.pow(x, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + \
+                  torch.pow(self.centers, 2).sum(dim=1, keepdim=True).expand(self.num_classes, batch_size).t()
+        distmat.addmm_(1, -2, x, self.centers.t())
+
+        classes = torch.arange(self.num_classes)
+        if self.use_gpu: classes = classes.cuda()
+        labels = labels.unsqueeze(1).expand(batch_size, self.num_classes)
+        mask = labels.eq(classes.expand(batch_size, self.num_classes))
+
+        dist = []
+        for i in range(batch_size):
+            value = distmat[i][mask[i]]
+            value = value.clamp(min=1e-12, max=1e+12)
+            dist.append(value)
+        dist = torch.cat(dist)
+        loss = dist.mean()
+        return loss
