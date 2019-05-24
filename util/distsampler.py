36de9a7ca8b23b1846659c5c1cb2a0d7dd522cf2
xiaozh818 <xiaozh_thu@163.com>
2019-05-24 09:30:55 -0400

    add distributed training code

---
+++ util/distsampler.py
@@ -0,0 +1,51 @@
+import torch.distributed as dist
+from torch.utils.data import Sampler
+from torch._six import int_classes as _int_classes
+import torch
+
+
+class distWeightedRandomSampler(Sampler):
+    r"""Samples elements from [0,..,len(weights)-1] with given probabilities (weights).
+
+    Args:
+        weights (sequence)   : a sequence of weights, not necessary summing up to one
+        num_samples (int): number of samples to draw
+        replacement (bool): if ``True``, samples are drawn with replacement.
+            If not, they are drawn without replacement, which means that when a
+            sample index is drawn for a row, it cannot be drawn again for that row.
+
+    Example:
+        >>> list(WeightedRandomSampler([0.1, 0.9, 0.4, 0.7, 3.0, 0.6], 5, replacement=True))
+        [0, 0, 0, 1, 0]
+        >>> list(WeightedRandomSampler([0.9, 0.4, 0.05, 0.2, 0.3, 0.1], 5, replacement=False))
+        [0, 1, 4, 3, 2]
+    """
+
+    def __init__(self, weights, num_samples, num_replicas=None, rank=None, replacement=True):
+        if not isinstance(num_samples, _int_classes) or isinstance(num_samples, bool) or \
+                num_samples <= 0:
+            raise ValueError("num_samples should be a positive integer "
+                             "value, but got num_samples={}".format(num_samples))
+        if not isinstance(replacement, bool):
+            raise ValueError("replacement should be a boolean value, but got "
+                             "replacement={}".format(replacement))
+        if num_replicas is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+            num_replicas = dist.get_world_size()
+        if rank is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+            rank = dist.get_rank() 
+        self.weights = torch.as_tensor(weights, dtype=torch.double)
+        self.num_samples = num_samples // num_replicas
+        self.replacement = replacement        
+        self.num_replicas = num_replicas
+        self.rank = rank
+
+    def __iter__(self):
+        return iter(torch.multinomial(self.weights, self.num_samples, self.replacement).tolist())
+
+    def __len__(self):
+        return self.num_samples
+
